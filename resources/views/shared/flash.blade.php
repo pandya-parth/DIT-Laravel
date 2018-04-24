7b1e62ca09acfd134338e549fb915d016aefc224
kajal pandya <kajal@krishaweb.net>
2018-04-24 17:22:55 +0530

    flash msg

--- resources/views/shared/flash.blade.php
+++ resources/views/shared/flash.blade.php
@@ -1,26 +1,26 @@
 @if(Session::has('success'))
-    <div class="alert alert-success alert-dismissible" style="margin-top: 60px;">
+    <div class="alert alert-success alert-dismissible">
         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
         <p><i class="icon fa fa-check"></i> Success! {{ Session::get('success') }}</p>        
     </div>
 @endif
 
 @if(Session::has('error'))
-    <div class="alert alert-danger alert-dismissible" style="margin-top: 60px;">
+    <div class="alert alert-danger alert-dismissible">
         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
         <p><i class="icon fa fa-ban"></i> Alert! {{ Session::get('error') }}</p>        
     </div>
 @endif
 
 @if(Session::has('info'))
-    <div class="alert alert-info alert-dismissible" style="margin-top: 60px;">
+    <div class="alert alert-info alert-dismissible">
         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
         <p><i class="icon fa fa-info"></i> Alert! {{ Session::get('info') }}</p>        
     </div>
 @endif
 
 @if(Session::has('warning'))
-    <div class="alert alert-warning alert-dismissible" style="margin-top: 60px;">
+    <div class="alert alert-warning alert-dismissible">
         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
         <p><i class="icon fa fa-warning"></i> Alert! {{ Session::get('warning') }}</p>        
     </div>
